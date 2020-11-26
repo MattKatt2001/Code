58b8af46576824ef4ab7b13f7e6fba2339961b51
MattKatt2001 <matthew.katsoulotos@gmail.com>
2020-11-26 13:52:09 +1100

    string reverse

---
+++ Random/String_reverse.py
@@ -0,0 +1,11 @@
+def order(x):
+    #y = []
+    i = len(x[0])
+    i = i - 1
+    while i >= 0:
+        #y.append(x[0][i])
+        print(x[0][i])
+        i -= 1
+    #print(y)
+
+x = order(["123456789"])#enter a string returns reversed
