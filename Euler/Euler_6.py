c82fe22765a7dfc92ce5ec0c4eed6dd99931e8f8
MattKatt2001 <matthew.katsoulotos@gmail.com>
2020-12-04 22:41:13 +1100

    Euler6

--- Euler/Euler_6.py
+++ Euler/Euler_6.py
@@ -1,1 +1,25 @@
 #https://projecteuler.net/problem=6
+
+x1 = []
+x2 = []
+for i in range (1,101):
+    x1.append(i)
+    x2.append(i**2)
+
+y1 = sum(x1)
+y2 = sum(x2)
+
+z = y1 ** 2
+
+
+#print(y1)
+#print(y2)
+#print(z)
+
+ans = z - y2
+print(ans)
+
+
+
+#solved
+#ans = 25164150
