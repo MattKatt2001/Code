2b24ce1dadcdd5c4b6eb129ab96e23685269a223
MattKatt2001 <matthew.katsoulotos@gmail.com>
2020-12-18 19:48:52 +1100

    Fix

--- Projects/Python_import/Card.py
+++ Projects/Python_import/Card.py
@@ -2,3 +2,6 @@ class card:
     def __init__(self,num,s):
         self.num = num
         self.s = s
+        self.deck = 52
+    def read_card(self):
+        print("The " + str(self.num) + " of " + self.s + " in a deck of " + str(self.deck))
--- Projects/Python_import/Main.py
+++ Projects/Python_import/Main.py
@@ -1,3 +1,4 @@
-from Card import card#  from the file called Card import the class called card
-card1 = card(10,"h")
-print(card1.num)
+from Card import* #card from the file called Card.py import the class card and its methods use an * for all
+card1 = card(10,"Hearts")
+card1.read_card()
+print(card1.deck)
