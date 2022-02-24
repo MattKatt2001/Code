8a8ec86895a4d730d89806d643b6f7d2dab4eb8b
MattKatt2001 <matthew.katsoulotos@gmail.com>
2022-02-24 17:46:58 +1100

    Basic template is done.

---
+++ Shooter/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "human.cpp"
+
+int main()
+{
+	human Matt("Matthew", 90, 10, 10);
+	human Joe("Joe", 100, 10, 5);
+	int mDamage = Matt.get_strength();
+	int jDamage = Joe.get_strength();
+
+	while(0==0)
+	{}
+}
